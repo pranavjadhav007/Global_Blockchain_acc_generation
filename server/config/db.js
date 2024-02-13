9c50b8a0e6b02668704dd8f330573d37386bef01
Pranav Jadhav <88050656+pranavjadhav007@users.noreply.github.com>
2024-02-13 23:17:15 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectdb= async ()=>{
     const res=await mongoose.connect(
-        "mongodb+srv://jocker:jocker@authenticat.iaovajk.mongodb.net/?retryWrites=true&w=majority"
+        "mongodb+srv://db:db@authenticat.iaovajk.mongodb.net/?retryWrites=true&w=majority"
     )
     if(res){
         console.log('connected successfully');
